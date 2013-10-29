040bd28e553294d54ba2623314c5fe17c6494d78
napatlue <napatlue@gmail.com>
2013-10-29 16:18:57 -0400

    initialize

---
+++ hw4-11641/src/Recommender.java
@@ -0,0 +1,20 @@
+
+public class Recommender {
+
+	public static void main(String[] argv) {
+		String trainFile = argv[0];
+		String testFile = argv[1];
+		String outputFile = argv[2];
+
+		// If you see these outputs, it means you have successfully compiled and run the code.
+		// Then you can remove these three lines if you want.
+		System.out.println("Training File : " + trainFile);
+		System.out.println("Test File : " + testFile);
+		System.out.println("Output File : " + outputFile);
+
+		// Implement your recommendation modules using trainFile and testFile.
+		// And output the prediction scores to outputFile.
+
+	}
+
+}
